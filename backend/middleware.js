d5319285b04616eb49eab9b8af9209ba120b7e31
Rakshit Yadav <yadavrakshit60@gmail.com>
2026-01-31 16:05:53 +0530

    seeting up middleware and jwt veriyfing tokens
    Signed-off-by: Rakshit Yadav <yadavrakshit60@gmail.com>

---
+++ backend/middleware.js
@@ -0,0 +1,14 @@
+const JWT_SECRET=require("../config.js")
+export function authMiddleware(req,res,next){
+const autheader= req.header.authorization
+if (!autheader || !autheader.startsWith("Bearer")){
+    res.status(403).json({})
+}
+const token = autheader.split(' ')[1] //auth header = Bearer <token> toh apne ne usko split krke token le liye 
+try{
+    const tokenverfication=jwt.verify(token,JWT_SECRET)
+    req.userid=tokenverfication.userid      // tokenverification.userid se kya hoga ki token verify hone ke bdd user nikalenge orr http ko denge ki ha ye user hai
+}catch{
+res.status(404).json()
+}
+}
